9aff4fb496070993237dd17b48602de9dac6a215
thutoaphiri <30575973+thutoaphiri@users.noreply.github.com>
2021-11-23 23:06:39 +0200

    animation added to contact card component.

--- my-online-portfolio-app/src/screens/HomePage/HomePage.jsx
+++ my-online-portfolio-app/src/screens/HomePage/HomePage.jsx
@@ -105,7 +105,7 @@ const HomePage = () => {
                 selectedSectionIcon={selectedSectionIcon}
                 closeSidePanel={closeSectionBodyPanel}
             />
-            <ContactCard />
+            <ContactCard runAnimation={runAnimation} />
         </div>
     );
 }
--- my-online-portfolio-app/src/screens/HomePage/components/SectionBodyPanel/components/ContactCard.jsx
+++ my-online-portfolio-app/src/screens/HomePage/components/SectionBodyPanel/components/ContactCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const ContactCard = () => {
+const ContactCard = props => {
     return (
-        <div className="cc-outer-container">
+        <div
+            className="cc-outer-container"
+            style={{
+                transform: `translateY(${props.runAnimation ?  "0%" : "150%"})`,
+            }}
+        >
             <text className="location">Johannesburg | South Africa</text>
             <div className="cc-contact-entry">
                 <text className="information">raykan.more@gmail.com</text>
